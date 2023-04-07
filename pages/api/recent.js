71ec87ea2e7e7f44003d9bd3f1d44859782ed5ff
Salo M <69745872+salo18@users.noreply.github.com>
2023-04-07 23:49:40 +1200

    move scraper to api

--- pages/api/recent.js
+++ pages/api/recent.js
@@ -1,7 +1,50 @@
 // import { NextApiRequest, NextApiResponse } from 'next'
 
-import { getLakeReading } from "../../lib/scraper";
+// import { getLakeReading } from "../../lib/scraper";
 const { MongoClient } = require("mongodb");
+
+
+import edgeChromium from 'chrome-aws-lambda'
+import puppeteer from 'puppeteer-core'
+
+// import puppeteer from "puppeteer";
+
+const LOCAL_CHROME_EXECUTABLE = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+
+
+async function getLakeReading() {
+  const executablePath = await edgeChromium.executablePath || LOCAL_CHROME_EXECUTABLE
+
+  const browser = await puppeteer.launch({
+    executablePath,
+    args: edgeChromium.args,
+    headless: false,
+  })
+
+  // const browser = await puppeteer.launch();
+
+  const page = await browser.newPage();
+
+  await page.goto(
+    "https://www.lawa.org.nz/explore-data/otago-region/swimming/lake-wakatipu-at-frankton-bay/swimsite"
+  );
+
+  const date = await page.$eval("#sample-date", (el) =>
+    el.innerText.split(" ").slice(2).join(" ")
+  );
+
+  const status = await page.$eval(".grade-icon--label", (el) =>
+    el.textContent.split(" ").slice(0, -4).join(" ")
+  );
+
+  browser.close();
+
+  return {
+    lawaSampleDate: date,
+    status,
+    cronUpdateDate: Date.now(),
+  };
+}
 
 const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri);
