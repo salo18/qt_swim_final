3a3da5c0e46f44b998ecbaa455d37dcca59007cd
Salo M <69745872+salo18@users.noreply.github.com>
2023-04-13 16:04:54 +1200

    t&c

---
+++ pages/privacy.js
@@ -0,0 +1,32 @@
+import Link from 'next/link';
+
+export default function Privacy() {
+  return (
+    <div className="container">
+      <Link href="/">Back to home</Link>
+      <h1>Privacy Policy</h1>
+
+      <p>At qtwateralert.co.nz, we respect the privacy of our users. This Privacy Policy explains how we collect, use, and disclose your personal information when you use our website (the "Website").</p>
+
+      <p>1. Information We Collect: We collect personal information that you voluntarily provide to us, such as your name and email address. We may also collect non-personal information, such as your IP address and browser type.</p>
+
+      <p>2. Use of Information: We use your personal information to communicate with you, to provide you with services or information that you request, and to improve the Website. We may also use your information for research or analytics purposes.</p>
+
+      <p>3. Disclosure of Information: We may share your personal information with third-party service providers who perform services on our behalf. We may also disclose your information if required by law or to protect our rights or the rights of others.</p>
+
+      <p>4. Cookies: The Website uses cookies to improve your experience and to collect non-personal information. You can disable cookies in your browser settings, but this may affect the functionality of the Website.</p>
+
+      <p>5. Third-Party Websites: The Website may contain links to third-party websites. We are not responsible for the privacy practices or content of those websites.</p>
+
+      <p>6. Children's Privacy: The Website is not intended for use by children under the age of 13. We do not knowingly collect personal information from children under the age of 13.</p>
+
+      <p>7. Security: We take reasonable measures to protect your personal information from unauthorized access or disclosure.</p>
+
+      <p>8. Changes to Privacy Policy: We may modify this Privacy Policy at any time without notice. Your continued use of the Website following any such modification constitutes your agreement to be bound by the modified Privacy Policy.</p>
+
+      <p>9. Contact Us: If you have any questions or concerns about this Privacy Policy, please contact us at [insert contact email address here].</p>
+
+      <p>By using the qtwateralert.co.nz website, you consent to the terms of this Privacy Policy. If you do not agree with any of these terms, please do not use the Website.</p>
+    </div>
+  );
+}
